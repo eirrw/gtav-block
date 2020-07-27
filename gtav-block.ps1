329eb4ea3a7f753928d4f7688e0b6aca8aeeb3d4
Virunus <tech+git@virunus.com>
2020-07-26 20:57:42 -0400

    initial commit

---
+++ gtav-block.ps1
@@ -0,0 +1,23 @@
+﻿# port 6672 inbound/outbound
+$ruleName = "GTAV Block"
+
+Write-Host "1 - Enable Firewall rules"
+Write-Host "2 - Disable Firewall rules"
+Write-Host "3 - View Firewall rules status"
+$selection = Read-Host "Select option"
+
+try {
+    if ($selection -eq 1) {
+        netsh advfirewall firewall set rule name="GTAV Block" new enable=yes
+    } elseif ($selection -eq 2) {
+        netsh advfirewall firewall set rule name="GTAV Block" new enable=no
+    } elseif ($selection -eq 3) {
+        netsh advfirewall firewall show rule name="GTAV Block"
+    } else {
+        Write-Host "Invalid option!"
+    }
+} catch {
+    Write-Error "Error!"
+}
+
+Read-Host "Press enter to quit"
